03bb9e6f03c06ac5403b15d4db8f4844d828a826
leontinepaq <lpaquatt@made-f0Ar2s11.clusters.42paris.fr>
2024-07-12 17:44:37 +0200

    modif file names

---
+++ ex01/PhoneBook.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook_class.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 17:26:58 by lpaquatt          #+#    #+#             */
+/*   Updated: 2024/07/12 17:02:46 by lpaquatt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+
+PhoneBook::PhoneBook(void)
+{
+	this->contact_to_write = 0;
+	return ;
+}
+
+PhoneBook::~PhoneBook(void)
+{
+	return ;
+}
+void	PhoneBook::add(void)
+{
+	this->contacts[this->contact_to_write].fill_contact(&this->contact_to_write);
+	return ;
+}
+
+void	PhoneBook::display_list_contacts(void)
+{
+	std::cout << "\nLIST OF CONTACTS:" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	std::cout << std::setw(10) << "Index" << "|";
+	std::cout << std::setw(10) << "First name" << "|";
+	std::cout << std::setw(10) << "Last name" << "|";
+	std::cout << std::setw(10) << "Nickname" << std::endl;
+	std::cout << "---------------------------------------------" << std::endl;
+	for (int i = 0; i < 8; i++)
+		this->contacts[i].display_contact_list(i);
+	std::cout << std::endl;
+}
+
+void	PhoneBook::select_contact(void)
+{
+	int		index;
+	std::string	input;
+
+	std::cout << "DISPLAY CONTACT:" << std::endl;
+	std::cout << "> Please enter the index of the contact you want to display: ";
+	std::getline(std::cin, input);
+	std::istringstream iss(input);
+    if (!(iss >> index) || !iss.eof()
+		|| index < 0 || index >= 8 || this->contacts[index].is_empty())
+    {
+        std::cout << "> Invalid index - no contact to display\n" << std::endl;
+        return;
+    }	
+	this->contacts[index].display_contact_information();
+	std::cout << std::endl;
+	return ;
+}
+
+void	PhoneBook::search(void)
+{
+	this->display_list_contacts();
+	this->select_contact();
+	return ;
+}
+
