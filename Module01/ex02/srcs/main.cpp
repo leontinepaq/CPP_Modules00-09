870de3cb6c951735daed4d2588171f3a68b71704
leontinepaq <lpaquatt@made-f0Ar2s7.clusters.42paris.fr>
2024-07-17 17:38:00 +0200

    ex02 first push

---
+++ Module01/ex02/srcs/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 17:25:15 by lpaquatt          #+#    #+#             */
+/*   Updated: 2024/07/17 17:35:22 by lpaquatt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "Address of the string:\t\t\t" << &str << std::endl;
+	std::cout << "Address of the string pointer:\t\t" << stringPTR << std::endl;
+	std::cout << "Address of the string reference:\t" << &stringREF << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "Value of the string:\t\t" << str << std::endl;
+	std::cout << "Value of the string pointer:\t" << *stringPTR << std::endl;
+	std::cout << "Value of the string reference:\t" << stringREF << std::endl;
+	return (0);
+}
