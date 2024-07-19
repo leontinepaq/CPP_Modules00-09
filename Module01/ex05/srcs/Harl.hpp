63930b1f2610b57f23c2bcdc07c19081c070b3d5
leontinepaq <lpaquatt@made-f0Ar2s7.clusters.42paris.fr>
2024-07-19 17:59:56 +0200

    ex05 first push

---
+++ Module01/ex05/srcs/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 17:01:08 by lpaquatt          #+#    #+#             */
+/*   Updated: 2024/07/19 17:05:00 by lpaquatt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+};
+
+#endif
---
+++ Module01/ex05/srcs/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 17:20:34 by lpaquatt          #+#    #+#             */
+/*   Updated: 2024/07/19 17:58:01 by lpaquatt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("BAD_LEVEL");
+	harl.complain("");
+	return (0);
+}
