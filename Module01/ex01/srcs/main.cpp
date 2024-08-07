4e56ded33632955e71959e7a78cecdfeee113701
leontinepaq <lpaquatt@bess-f3r2s5.clusters.42paris.fr>
2024-08-07 13:36:41 +0200

    main with explanations

--- Module01/ex01/srcs/main.cpp
+++ Module01/ex01/srcs/main.cpp
@@ -6,36 +6,41 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 15:46:04 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/07/17 17:23:02 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/08/07 13:34:22 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-
-# include <iostream>
-# include <cstdlib>
-# include <string>
-# include <iomanip>
-# include <limits>
-# include <sstream> 
-
+#define BLUE "\033[0;34m"
+#define BOLD "\033[1m"
+#define RESET "\033[0m"
 
 int main(void)
 {
-	int N = 6;
-	
-	std::cout << "*Creating a horde of " << N << " zombies*" << std::endl;
-	Zombie* zombie = zombieHorde(N, "Martin");
+	int N;
+	std::cout << BLUE << "Enter the number of zombies you want to create: ";	
+	std::cin >> N;
+	std::string name;
+	std::cout << "Enter a name for the horde: ";
+	std::cin >> name;
+	std::cout << RESET;
+	std::cout << std::endl;
+
+	std::cout << BOLD << "*Creating a horde of " << N << " zombies*" << RESET << std::endl;
+	Zombie* zombie = zombieHorde(N, name);
 	std::cout << std::endl;
 	if (!zombie)
 		return (1);
+
 	for (int i = 0; i < N; i++)
 	{
-		std::cout << "*Announce from zombie " << i + 1 << "*" << std::endl;
+		std::cout << BOLD << "*Announce from zombie " << i + 1 << "*" << RESET << std::endl;
 		zombie[i].announce();
-		std::cout << std::endl;
 	}
-	std::cout << "*Deleting the horde*" << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << BOLD << "*Deleting the horde*" << RESET << std::endl;
 	delete [] zombie;
+	return (0);
 }
