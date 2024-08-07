b9a6c33831343a2206c3076a8b8f14a9cd641b9d
leontinepaq <lpaquatt@bess-f3r2s5.clusters.42paris.fr>
2024-08-07 15:27:23 +0200

    more use of switch

--- Module01/ex06/srcs/main.cpp
+++ Module01/ex06/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:20:34 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/08/07 15:15:43 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/08/07 15:27:16 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,3 @@ int main(int ac, char **av)
 	harl.complain(av[1]);
 	return (0);
 }
-
-
-// int main(int ac, char **av)
-// {
-// 	Harl		harl;
-// 	std::string levels[NB_LEVELS] = LEVELS;
-// 	int			level;
-	
-// 	if (ac != 2)
-// 	{
-// 		std::cout << "Usage: ./HarlFilter [DEBUG/INFO/WARNING/ERROR]" << std::endl;
-// 		return (1);
-// 	}
-// 	level = getLevel(av[1]);
-// 	switch (level)
-// 	{
-// 		case (-1):
-// 			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-// 			break;
-// 		default:
-// 			harl.complain(levels[level]);
-// 			break;
-// 	}
-// 	return (0);
-// }
