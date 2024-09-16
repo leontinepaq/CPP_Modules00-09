79e59e7aabb47cc5c86d021fbe0255b27b3d5fe6
leontinepaq <lpaquatt@made-f0Ar3s11.clusters.42paris.fr>
2024-09-16 19:00:35 +0200

    modif colors en static

--- Module08/ex00/include/Colors.hpp
+++ Module08/ex00/include/Colors.hpp
@@ -6,40 +6,26 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 17:04:01 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/09/16 17:24:53 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/09/16 18:39:20 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_HPP
-#define COLORS_HPP
+# define COLORS_HPP
 
-#include <string>
+# include <string>
 
-class Colors {
-public:
-    static const std::string RED;
-    static const std::string GREEN;
-    static const std::string CYAN;
-    static const std::string MAGENTA;
-    static const std::string YELLOW;
-    static const std::string GREY;
-    static const std::string WHITE;
-    static const std::string ORANGE;
-    static const std::string PURPLE;
-    static const std::string BLUE;
-    static const std::string RESET;
-};
+static const std::string RED = "\033[1;31m";
+static const std::string GREEN = "\033[1;32m";
+static const std::string CYAN = "\033[1;36m";
+static const std::string MAGENTA = "\033[1;35m";
+static const std::string YELLOW = "\033[1;33m";
+static const std::string GREY = "\033[1;30m";
+static const std::string WHITE = "\033[1;37m";
+static const std::string ORANGE = "\033[1;38;5;208m";
+static const std::string PURPLE = "\033[1;38;5;129m";
+static const std::string BLUE = "\033[1;34m";
+static const std::string RESET = "\033[0m";
 
-const std::string Colors::RED = "\033[1;31m";
-const std::string Colors::GREEN = "\033[1;32m";
-const std::string Colors::CYAN = "\033[1;36m";
-const std::string Colors::MAGENTA = "\033[1;35m";
-const std::string Colors::YELLOW = "\033[1;33m";
-const std::string Colors::GREY = "\033[1;30m";
-const std::string Colors::WHITE = "\033[1;37m";
-const std::string Colors::ORANGE = "\033[1;38;5;208m";
-const std::string Colors::PURPLE = "\033[1;38;5;129m";
-const std::string Colors::BLUE = "\033[1;34m";
-const std::string Colors::RESET = "\033[0m";
 
 #endif
--- Module08/ex00/srcs/main.cpp
+++ Module08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 12:52:06 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/09/16 17:24:29 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/09/16 19:00:08 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,6 @@
 #include <vector>
 #include <list>
 
-
-// #ifndef COLORS
-// # define BLUE		"\033[1;34m"
-// # define Colors::RED		"\033[1;31m"
-// # define Colors::GREEN		"\033[1;32m"
-// # define Colors::CYAN		"\033[1;36m"
-// # define MAGENTA	"\033[1;35m"
-// # define YELLOW		"\033[1;33m"
-// # define GREY		"\033[1;30m"
-// # define Colors::WHITE		"\033[1;37m"
-// # define ORANGE		"\033[1;38;5;208m"
-// # define PURPLE		"\033[1;38;5;129m"
-// # define Colors::RESET		"\033[0m"
-// #endif
 
 
 
@@ -53,83 +39,83 @@ std::list<int> valuesToList (int values[], size_t n)
 
 int main()
 {
-	std::cout << Colors::CYAN << "TEST WITH INT VECTOR" << Colors::RESET << std::endl;
+	std::cout << CYAN << "TEST WITH INT VECTOR" << RESET << std::endl;
 	int values[] = {1, 500, 42, -1};
 	size_t n = sizeof(values) / sizeof(values[0]);
 	std::vector<int> tab = valuesToVector(values, n);
 	
-	std::cout << Colors::WHITE << "tab = [ ";
+	std::cout << WHITE << "tab = [ ";
 	for (std::vector<int>::iterator it = tab.begin(); it != tab.end(); ++it)
 		std::cout << *it << " ";
-	std::cout << "]" << Colors::RESET << std::endl;
+	std::cout << "]" << RESET << std::endl;
 
 	try
 	{
 		std::vector<int>::iterator toFind; 
-		std::cout << Colors::WHITE << "> Trying to find 42 in the vector: " << Colors::RESET;
+		std::cout << WHITE << "> Trying to find 42 in the vector: " << RESET;
 		toFind = easyfind(tab, 42);
-		std::cout << Colors::GREEN << *toFind << " was found at index " 
-			<< toFind - tab.begin() << Colors::RESET << std::endl; 
-		std::cout << Colors::WHITE << "> Trying to find 0 in the vector: " << Colors::RESET;
+		std::cout << GREEN << *toFind << " was found at index " 
+			<< toFind - tab.begin() << RESET << std::endl; 
+		std::cout << WHITE << "> Trying to find 0 in the vector: " << RESET;
 		toFind = easyfind(tab, 0);
-		std::cout << Colors::GREEN << *toFind << " was found at index " 
-			<< toFind - tab.begin() << Colors::RESET << std::endl;
+		std::cout << GREEN << *toFind << " was found at index " 
+			<< toFind - tab.begin() << RESET << std::endl;
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << Colors::RED << "Exception caught: " << e.what() << Colors::RESET << std::endl;
+		std::cerr << RED << "Exception caught: " << e.what() << RESET << std::endl;
 	}
 	std::cout << std::endl;
 
-	std::cout << Colors::CYAN << "TEST WITH LIST" << Colors::RESET << std::endl;
+	std::cout << CYAN << "TEST WITH LIST" << RESET << std::endl;
 	int values2[] = {0, 1, -1, 42, 50, 42};
 	n = sizeof(values2) / sizeof(values2[0]);
 	std::list<int> list = valuesToList(values, n);
 
-	std::cout << Colors::WHITE<< "list = [ ";
+	std::cout << WHITE<< "list = [ ";
 	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
 		std::cout << *it << " ";
-	std::cout << "]" << Colors::RESET << std::endl;
+	std::cout << "]" << RESET << std::endl;
 	try
 	{
 		std::list<int>::iterator toFind;
-		std::cout << Colors::WHITE << "> Trying to find 42 in the list: " << Colors::RESET;
+		std::cout << WHITE << "> Trying to find 42 in the list: " << RESET;
 		toFind = easyfind(list, 42);
-		std::cout << Colors::GREEN << *toFind << " was found at index " 
-			<< std::distance(list.begin(), toFind) << Colors::RESET << std::endl;
-		std::cout << Colors::WHITE << "> Trying to find 0 in the list: " << Colors::RESET;
+		std::cout << GREEN << *toFind << " was found at index " 
+			<< std::distance(list.begin(), toFind) << RESET << std::endl;
+		std::cout << WHITE << "> Trying to find 0 in the list: " << RESET;
 		toFind = easyfind(list, 0);
-		std::cout << Colors::GREEN << *toFind << " was found at index " 
-			<< std::distance(list.begin(), toFind) << Colors::RESET << std::endl;		
+		std::cout << GREEN << *toFind << " was found at index " 
+			<< std::distance(list.begin(), toFind) << RESET << std::endl;		
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << Colors::RED << "Exception caught: " << e.what() << Colors::RESET << std::endl;
+		std::cerr << RED << "Exception caught: " << e.what() << RESET << std::endl;
 	}
 	std::cout << std::endl;
 }
 
 
-	// std::cout << Colors::CYAN << "TEST WITH STRING VECTOR" << Colors::RESET << std::endl;
+	// std::cout << CYAN << "TEST WITH STRING VECTOR" << RESET << std::endl;
 	// std::vector<std::string> tab2;
 	// tab2.push_back("Hello");
 	// tab2.push_back("World");
 	// tab2.push_back("42");
 	
-	// std::cout << Colors::WHITE "tab2 = [ ";
+	// std::cout << WHITE "tab2 = [ ";
 	// for (std::vector<std::string>::iterator it = tab2.begin(); it != tab2.end(); ++it)
 	// 	std::cout << *it << " ";
-	// std::cout << "]" Colors::RESET << std::endl;
+	// std::cout << "]" RESET << std::endl;
 	
-	// std::cout<< Colors::RED "> Does not compile <" Colors::RESET << std::endl;
+	// std::cout<< RED "> Does not compile <" RESET << std::endl;
 	// // try
 	// // {
-	// // 	std::cout << Colors::WHITE "> Trying to find 42 in the vector: " Colors::RESET;
-	// // 	std::cout << Colors::GREEN << *easyfind(tab2, 42) << " was found" Colors::RESET << std::endl;
+	// // 	std::cout << WHITE "> Trying to find 42 in the vector: " RESET;
+	// // 	std::cout << GREEN << *easyfind(tab2, 42) << " was found" RESET << std::endl;
 	// // }
 	// // catch(const std::exception& e)
 	// // {
-	// // 	std::cerr << Colors::RED "Exception caught: " << e.what() << Colors::RESET << std::endl;
+	// // 	std::cerr << RED "Exception caught: " << e.what() << RESET << std::endl;
 	// // }
 // }
 
