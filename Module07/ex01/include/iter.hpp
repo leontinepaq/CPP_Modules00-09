33ac20a96e95de1f724e5c0eee964c8f9d821cad
leontinepaq <lpaquatt@made-f0Ar3s7.clusters.42paris.fr>
2024-09-25 13:03:08 +0200

    typename pour la fct

--- Module07/ex01/include/iter.hpp
+++ Module07/ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 17:18:42 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/09/09 17:49:39 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/09/23 13:57:40 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 #include <cstddef>
 
-template< typename T >
-void	iter(T *array, std::size_t length, void (*function)(T &))
+template< typename T, typename F >
+void	iter(T *array, std::size_t length, F *f)
 {
 	for (std::size_t i = 0; i < length; i++)
-		function(array[i]);
+		f(array[i]);
 }
 
 #endif
--- Module07/ex01/srcs/main.cpp
+++ Module07/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lpaquatt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 17:45:52 by lpaquatt          #+#    #+#             */
-/*   Updated: 2024/09/09 18:03:11 by lpaquatt         ###   ########.fr       */
+/*   Updated: 2024/09/24 12:11:09 by lpaquatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,10 @@ void add_one(int &i)
 {
 	i++;
 }
-void add_one(char &c)
+
+void replaceByC(char &c)
 {
-	c++;
+	c = 'c';
 }
 
 template< typename T >
@@ -37,18 +38,18 @@ int main()
 	std::cout << CYAN << "Testing with int array" << RESET << std::endl;
 	int array[] = {1, 2, 3, 4, 5};
 	std::cout << BLUE << "Print array:" << RESET << std::endl;
-	iter(array, 5, &print);
+	iter(array, 5, &print<int>);
 	std::cout << BLUE << "Add one to each element:" << RESET << std::endl;
 	iter(array, 5, &add_one);
-	iter(array, 5, &print);
+	iter(array, 5, &print<int>);
 	std::cout << std::endl;
 
 	std::cout << CYAN << "Testing with char array" << RESET << std::endl;
 	char array2[] = {'a', 'b', 'c', 'd', 'e'};
 	std::cout << BLUE << "Print array:" << RESET << std::endl;
-	iter(array2, 5, &print);
-	std::cout << BLUE << "Add one to each element:" << RESET << std::endl;
-	iter(array2, 5, &add_one);
-	iter(array2, 5, &print);
+	iter(array2, 5, &print<char>);
+	std::cout << BLUE << "Replace each element by c:" << RESET << std::endl;
+	iter(array2, 5, &replaceByC);
+	iter(array2, 5, &print<char>);
 	std::cout << std::endl;
 }
